717ffff29ffd8ff8e200715209ba1e5efbb80e53
Infarh <shmachilin@yandex.ru>
2021-01-19 21:21:17 +0300

    Макеты

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -8,23 +8,6 @@ namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
-        private static readonly List<Employee> __Employees = new()
-        {
-            new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 37 },
-            new Employee { Id = 2, LastName = "Петров", FirstName = "Пётр", Patronymic = "Петрович", Age = 25 },
-            new Employee { Id = 3, LastName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", Age = 27 },
-        };
-
-        public IActionResult Index() => View(/*"SecondView"*/);
-
-        public IActionResult SecondAction()
-        {
-            return Content("Second controller action");
-        }
-
-        public IActionResult Employees()
-        {
-            return View(__Employees);
-        }
+        public IActionResult Index() => View();
     }
 }
